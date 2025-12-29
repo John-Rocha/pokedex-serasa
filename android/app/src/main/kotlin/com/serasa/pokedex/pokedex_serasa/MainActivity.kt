4b9b1e49a50a867fea888b6cebeda74c49e4da92
Johnathan Rocha <johnathanrocha@gmail.com>
2025-12-29 20:10:18 -0300

    feat: implement Firebase Analytics integration with method channel for event logging

--- android/app/src/main/kotlin/com/serasa/pokedex/pokedex_serasa/MainActivity.kt
+++ android/app/src/main/kotlin/com/serasa/pokedex/pokedex_serasa/MainActivity.kt
@@ -1,5 +1,21 @@
 package com.serasa.pokedex.pokedex_serasa
 
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.serasa.pokedex.analytics.AnalyticsMethodChannel
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val analyticsChannel = AnalyticsMethodChannel(analytics)
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            "com.serasa.pokedex/analytics"
+        ).setMethodCallHandler(analyticsChannel)
+    }
+}
