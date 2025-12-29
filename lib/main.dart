1ab2d04bdc0c56ca54e0f425b138bce36a4f0311
Johnathan Rocha <johnathanrocha@gmail.com>
2025-12-29 20:10:40 -0300

    feat: initialize Firebase in main function for app setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:pokedex_serasa/app/app_module.dart';
 import 'package:pokedex_serasa/app/app_widget.dart';
+import 'package:pokedex_serasa/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   return runApp(
     ModularApp(
       module: AppModule(),
